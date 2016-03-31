910ea382052044d83f8d97dce009c04c7d21e418
Joe <joe.chen@rice.edu>
2016-03-30 22:15:09 -0500

    Update uWaveQuant.m to be multi-dimensional

--- Code/uWaveMatlab/functions/uWaveQuant.m
+++ Code/uWaveMatlab/functions/uWaveQuant.m
@@ -1,6 +1,6 @@
 function [t,Q] = uWaveQuant(tIn,d)
     %Constants
-    windowSize = 0.050; %milliseconds
+    windowSize = 50; %milliseconds
     stepSize = 30; %milliseconds
     convertWindow = windowSize/1000; %window size in seconds
     convertStep = stepSize/1000; %step size in seconds
@@ -9,10 +9,13 @@ function [t,Q] = uWaveQuant(tIn,d)
     t=min(tIn)+convertStep:convertStep:max(tIn); %time values out (s)
     
     %Iterate through reduced time points to produce average
+    Q = zeros(length(tIn),3);
     for ind = 1:length(t)
-        curT = t(ind); %Current time
-        curInds = find(tIn <= curT & tIn >= curT-convertWindow); %Find indices within time window
-        curD = d(curInds); %Associated time series data
-        Q(ind) = mean(curD); %Find mean
+        for k = 1:3
+            curT = t(ind); %Current time
+            curInds = find(tIn <= curT & tIn >= curT-convertWindow); %Find indices within time window
+            curD = d(curInds,k); %Associated time series data
+            Q(ind,k) = mean(curD); %Find mean
+        end
     end
 end
