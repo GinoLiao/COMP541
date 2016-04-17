0dc6685dff051731d5690dab449a9daedd9df656
Joe <joe.chen@rice.edu>
2016-04-16 21:49:59 -0500

    Update uWaveQuant for Android->IOS downconversion

--- Code/uWaveMatlab/functions/postprocess/CalibrationCellMatrix.m
+++ Code/uWaveMatlab/functions/postprocess/CalibrationCellMatrix.m
@@ -1,7 +1,13 @@
 function cm = CalibrationCellMatrix(totalGestures, numRuns)
     cm = cell(17,5);
-    for indGes = 1:totalGestures
+    range=1:totalGestures;
+    excludeInds = [3 4];
+    tic;
+    for indGes = setxor(range,excludeInds)
+        display(indGes);
         [timeseries, indo, time, dtwmean, dtwstd] = GetOneCalibration('calibration/',indGes,numRuns);
+        toc;
         cm(indGes,:) = {timeseries, indo, time, dtwmean, dtwstd};
+        toc;
     end
 end
--- Code/uWaveMatlab/functions/uWaveQuant.m
+++ Code/uWaveMatlab/functions/uWaveQuant.m
@@ -1,18 +1,18 @@
 function [t,Q] = uWaveQuant(tIn,d)
     %Constants
-    windowSize = 500; %milliseconds
-    stepSize = 150; %milliseconds
+    windowSize = 100; %milliseconds
+    stepSize = 100; %milliseconds
     convertWindow = windowSize/1000; %window size in seconds
     convertStep = stepSize/1000; %step size in seconds
     
     %Produce reduced time points
-    t=min(tIn)+convertStep:convertStep:max(tIn); %time values out (s)
+    t=min(tIn):convertStep:max(tIn); %time values out (s)
     
     %Iterate through reduced time points to produce average
     Q = zeros(length(t),3);
     for ind = 1:length(t)
         curT = t(ind); %Current time
-        curInds = find(tIn <= curT & tIn >= curT-convertWindow); %Find indices within time window
+        curInds = find(tIn <= curT+convertWindow/2 & tIn >= curT-convertWindow/2); %Find indices within time window
         for k = 1:3
             curD = d(curInds,k); %Associated time series data
             Q(ind,k) = mean(curD); %Find mean
